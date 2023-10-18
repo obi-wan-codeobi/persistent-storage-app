d2c66684a048f0f0b104ac0158f3d797d5dfa0b6
obi-wan-codeobi <114459130+obi-wan-codeobi@users.noreply.github.com>
2023-10-18 11:49:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,8 @@
 
 <div class="image-grid">
     <?php
-    $dir = '/var/lib/data/galleries/';
+    //$dir = '/var/lib/data/galleries/';
+    $dir = '/galleries/';
     $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
 
     if (is_dir($dir)) {
